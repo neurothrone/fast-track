f86ffc8f433d6d0f7fca407cbe4e5a43b63e608d
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2022-11-08 04:13:13 +0100

    Initial Commit

---
+++ CortexOfFasting/CortexOfFastingApp.swift
@@ -0,0 +1,17 @@
+//
+//  CortexOfFastingApp.swift
+//  CortexOfFasting
+//
+//  Created by Zaid Neurothrone on 2022-11-08.
+//
+
+import SwiftUI
+
+@main
+struct CortexOfFastingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
