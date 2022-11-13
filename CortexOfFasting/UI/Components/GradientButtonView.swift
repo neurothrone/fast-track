e7c21d693a79ec900c58d474eb56280182e77370
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2022-11-13 15:17:08 +0100

    add unfinished custom gradient button to source control

---
+++ CortexOfFasting/UI/Components/GradientButtonView.swift
@@ -0,0 +1,48 @@
+//
+//  GradientButtonView.swift
+//  CortexOfFasting
+//
+//  Created by Zaid Neurothrone on 2022-11-10.
+//
+
+import SwiftUI
+
+struct GradientButtonView: View {
+  let label: String
+  let action: () -> Void
+  
+  var body: some View {
+    Button(action: action) {
+      Text(label)
+        .foregroundColor(.primary)
+    }
+    .padding()
+    .contentShape(RoundedRectangle(cornerRadius: 20))
+    .background(
+      RoundedRectangle(cornerRadius: 20)
+        .fill(.ultraThinMaterial)
+    )
+    .background(
+      LinearGradient(
+        colors: [.purple, .purple.opacity(0.5)],
+        startPoint: .leading,
+        endPoint: .trailing
+      )
+      .cornerRadius(20)
+    )
+    .background(
+      RoundedRectangle(cornerRadius: 20)
+        .stroke(.purple.gradient, lineWidth: 1)
+    )
+    .shadow(color: .purple, radius: 2)
+    .shadow(color: .purple.opacity(0.75), radius: 2)
+    .shadow(color: .purple.opacity(0.5), radius: 2)
+  }
+}
+
+struct GradientButtonView_Previews: PreviewProvider {
+  static var previews: some View {
+    GradientButtonView(label: "Start", action: {})
+      .preferredColorScheme(.dark)
+  }
+}
