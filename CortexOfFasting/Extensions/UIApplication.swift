f7f09100cf1a7e7b378eb03c0eb9a10e68bcc3c2
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2022-11-14 18:25:04 +0100

    add extension to get version number as string

---
+++ CortexOfFasting/Extensions/UIApplication.swift
@@ -0,0 +1,14 @@
+//
+//  UIApplication.swift
+//  CortexOfFasting
+//
+//  Created by Zaid Neurothrone on 2022-11-14.
+//
+
+import UIKit
+
+extension UIApplication {
+  static var appVersion: String {
+    Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String ?? ""
+  }
+}
