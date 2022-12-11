9ec97ec3a88dc1391a81607edcef79e382172d87
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2022-12-11 09:41:58 +0100

    add reusable row view for sectioned lists

---
+++ Shared/UI/All Weeks/LogSectionedListRowView.swift
@@ -0,0 +1,34 @@
+//
+//  LogSectionedListRowView.swift
+//  CortexOfFasting
+//
+//  Created by Zaid Neurothrone on 2022-12-11.
+//
+
+import SwiftUI
+
+struct LogSectionedListRowView: View {
+  let yearWeekLabel: String
+  let totalFastedStateDuration: Double
+  
+  var body: some View {
+    HStack {
+      Text(yearWeekLabel)
+        .foregroundColor(.mint)
+      
+      Spacer()
+      
+      Text("\(String(format: "%.1f", totalFastedStateDuration)) h")
+        .foregroundColor(.purple)
+    }
+  }
+}
+
+struct LogSectionedListRowView_Previews: PreviewProvider {
+  static var previews: some View {
+    LogSectionedListRowView(
+      yearWeekLabel: "2022 (Week 49)",
+      totalFastedStateDuration: 29.2
+    )
+  }
+}
