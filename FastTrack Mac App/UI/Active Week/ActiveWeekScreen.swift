1d916292883c4a237a958bc33e78619338cb64d2
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2022-12-11 09:43:49 +0100

    finish initial design for ActiveWeekScreen for mac

--- FastTrack Mac App/UI/Active Week/ActiveWeekScreen.swift
+++ FastTrack Mac App/UI/Active Week/ActiveWeekScreen.swift
@@ -8,13 +8,40 @@
 import SwiftUI
 
 struct ActiveWeekScreen: View {
+  @Environment(\.managedObjectContext) private var viewContext
+
+  @FetchRequest(
+    fetchRequest: FastLog.allCompletedInCurrentWeek,
+    animation: .default
+  )
+  private var fastLogs: FetchedResults<FastLog>
+  
   var body: some View {
-    Text("Active Week")
+    List {
+      Section {
+        if fastLogs.isEmpty {
+          Text("No logs in active week yet.")
+        } else {
+          ForEach(fastLogs) { log in
+            LogListRowView(log: log)
+              .listRowSeparator(.visible)
+              .listRowSeparatorTint(.white.opacity(0.2), edges: .all)
+              .alignmentGuide(.listRowSeparatorLeading) { viewDimensions in
+                return .zero
+              }
+          }
+        }
+      } header: {
+        SectionHeaderView(leftText: "Fasting times", rightText: "Fasting duration")
+      }
+    }
+    .listStyle(.plain)
   }
 }
 
 struct ActiveWeekScreen_Previews: PreviewProvider {
   static var previews: some View {
     ActiveWeekScreen()
+      .environment(\.managedObjectContext, CoreDataProvider.preview.viewContext)
   }
 }
