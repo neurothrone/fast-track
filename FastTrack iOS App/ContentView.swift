ceacb133611987ee52dae15b590c9713fe8ce9cf
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2022-12-12 06:28:36 +0100

    add two column layout for iPad and iOS pro max landscape orientation

--- FastTrack iOS App/ContentView.swift
+++ FastTrack iOS App/ContentView.swift
@@ -8,21 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
+  @Environment(\.colorScheme) var colorScheme
+  @Environment(\.horizontalSizeClass) var sizeClass
+  
   @AppStorage(Constants.AppStorage.selectedScreen)
   private var selectedTab: Screen = .activeWeek
   
+  @State private var selectedScreen: Screen? = .activeWeek
+  @State private var columnVisibility: NavigationSplitViewVisibility = .doubleColumn
+  
   var body: some View {
-    TabView(selection: $selectedTab) {
-      Group {
-        ForEach(Screen.allCases) { screen in
-          screen.view()
-            .tabItem {
-              Label(screen.rawValue, systemImage: screen.systemImage)
-            }
+    if sizeClass == .compact {
+      TabView(selection: $selectedTab) {
+        Group {
+          ForEach(Screen.allCases) { screen in
+            screen.view(colorScheme: colorScheme)
+              .tabItem {
+                Label(screen.rawValue, systemImage: screen.systemImage)
+              }
+          }
+        }
+        .toolbarBackground(.visible, for: .tabBar)
+        .toolbarBackground(.ultraThinMaterial, for: .tabBar)
+      }
+    } else {
+      NavigationSplitView(columnVisibility: $columnVisibility) {
+        List(selection: $selectedScreen) {
+          ForEach(Screen.allCases) { screen in
+            Label(screen.rawValue, systemImage: screen.systemImage)
+              .bold(screen == selectedScreen)
+              .foregroundColor(screen == selectedScreen ? .purple : .white.opacity(0.85))
+          }
+          .listRowBackground(Color.black)
+          .listRowSeparatorTint(.white.opacity(0.4))
+        }
+        .listStyle(.insetGrouped)
+        .scrollContentBackground(.hidden)
+        .scrollDisabled(true)
+        .linearBackground(colorScheme: colorScheme)
+      } detail: {
+        if let selectedScreen {
+          selectedScreen.view(colorScheme: colorScheme)
+        } else {
+          Text("Please select a screen.")
         }
       }
-      .toolbarBackground(.visible, for: .tabBar)
-      .toolbarBackground(.ultraThinMaterial, for: .tabBar)
+      .navigationSplitViewStyle(.balanced)
     }
   }
 }
