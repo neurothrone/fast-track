5e0b2110f03fc84a7b93c84fa7c4465dae1d74fb
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2022-12-13 09:58:46 +0100

    implement deletion for section of logs in iOS and macOS

--- Shared/UI/All Weeks/LogSectionedListView.swift
+++ Shared/UI/All Weeks/LogSectionedListView.swift
@@ -7,54 +7,112 @@
 
 import SwiftUI
 
+//MARK: - Equatable and Hashable conformance so selection is possible on macOS
+#if os(macOS)
+extension SectionedFetchResults<String, FastLog>.Element: Equatable, Hashable {
+  public static func ==(lhs: SectionedFetchResults<String, FastLog>.Element, rhs: SectionedFetchResults<String, FastLog>.Element) -> Bool {
+    lhs.id == rhs.id
+  }
+  
+  public func hash(into hasher: inout Hasher) {
+    hasher.combine(self.id)
+  }
+}
+#endif
+
 struct LogSectionedListView: View {
+  @Environment(\.managedObjectContext) var moc
+  
+#if os(macOS)
+  @State private var selectedSection: SectionedFetchResults<String, FastLog>.Element?
+#endif
+  
   let logsPerWeek: SectionedFetchResults<String, FastLog>
   
   var body: some View {
+#if os(iOS)
     List {
-      Section {
-        if logsPerWeek.isEmpty {
-          Text("No logs yet.")
-#if os(iOS)
-            .foregroundColor(.mint)
-            .listRowBackground(Color.black)
+      content
+    }
+    .scrollContentBackground(.hidden)
+#else
+    List(selection: $selectedSection) {
+      content
+    }
+    .listStyle(.plain)
+    .toolbar {
+      Button(role: .destructive, action: deleteSelected) {
+        Label("Delete", systemImage: "trash")
+      }
+      .keyboardShortcut(.delete, modifiers: [])
+      .disabled(selectedSection == nil)
+    }
 #endif
-        } else {
-          ForEach(logsPerWeek) { logsInWeek in
+  }
+  
+  private var content: some View {
+    Section {
+      if logsPerWeek.isEmpty {
+        Text("No logs yet.")
 #if os(iOS)
-            NavigationLink {
-              WeekDetailScreen(logs: logsInWeek)
-            } label: {
-              LogSectionedListRowView(
-                yearWeekLabel: logsInWeek.id,
-                totalFastedStateDuration: FastLog.totalFastedStateDurationToHours(
-                  in: Array(logsInWeek))
-              )
-            }
-            .listRowBackground(Color.black)
-#elseif os(macOS)
+          .foregroundColor(.mint)
+          .listRowBackground(Color.black)
+#endif
+      } else {
+        ForEach(logsPerWeek) { logsInWeek in
+#if os(iOS)
+          NavigationLink {
+            WeekDetailScreen(logs: logsInWeek)
+          } label: {
             LogSectionedListRowView(
               yearWeekLabel: logsInWeek.id,
               totalFastedStateDuration: FastLog.totalFastedStateDurationToHours(
                 in: Array(logsInWeek))
             )
-            .listRowSeparator(.visible)
-            .listRowSeparatorTint(.white.opacity(0.2), edges: .all)
-            .alignmentGuide(.listRowSeparatorLeading) { viewDimensions in
-              return .zero
-            }
-#endif
           }
-        }
-      } header: {
-        SectionHeaderView(leftText: "Year (Week)", rightText: "Fasted state hours")
-      }
-    }
-#if os(iOS)
-    .scrollContentBackground(.hidden)
+          .listRowBackground(Color.black)
 #elseif os(macOS)
-    .listStyle(.plain)
+          LogSectionedListRowView(
+            yearWeekLabel: logsInWeek.id,
+            totalFastedStateDuration: FastLog.totalFastedStateDurationToHours(
+              in: Array(logsInWeek))
+          )
+          .listRowSeparator(.visible)
+          .listRowSeparatorTint(.white.opacity(0.2), edges: .all)
+          .alignmentGuide(.listRowSeparatorLeading) { viewDimensions in
+            return .zero
+          }
+          .contextMenu {
+            Button("Delete", role: .destructive) {
+              FastLog.deleteAll(at: logsInWeek, using: moc)
+            }
+          }
+          .tag(logsInWeek)
 #endif
+        }
+#if os(iOS)
+        .onDelete(perform: delete)
+#endif
+      }
+    } header: {
+      SectionHeaderView(leftText: "Year (Week)", rightText: "Fasted state hours")
+    }
+  }
+}
+
+extension LogSectionedListView {
+#if os(macOS)
+  private func deleteSelected() {
+    if let selectedSection {
+      FastLog.deleteAll(at: selectedSection, using: moc)
+    }
+  }
+#endif
+  
+  private func delete(at offsets: IndexSet) {
+    guard let index = offsets.first else { return }
+    
+    FastLog.deleteAll(at: logsPerWeek[index], using: moc)
   }
 }
 
